230f8ef778d9eb2c7035115b840e401890e94ab0
20210181_김민성 <20210181@kumoh.ac.kr>
2025-11-23 03:53:38 +0900

    일단 경고뜨는거 고친거
    일단 경고뜨는거 고친거

--- Assets/Scripts/QuitController.cs
+++ Assets/Scripts/QuitController.cs
@@ -1,27 +1,27 @@
-using UnityEngine;
+//using UnityEngine;
 
-public class QuitController : MonoBehaviour
-{
-    public GameObject panel;
+//public class QuitController : MonoBehaviour
+//{
+//    public GameObject panel;
 
-    public void TogglePanel_()
-    {
-        if (panel != null)
-            panel.SetActive(!panel.activeSelf);
-        else
-            Debug.LogWarning("TogglePanelOnClick: panel is not assigned.");
-    }
-    public void Quit()
-    {
-        Application.Quit();
-    } 
-    void Update()
-    {
-        // #if UNITY_ANDROID
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            TogglePanel_();
-        }
-        // #endif
-    }
-}
+//    public void TogglePanel_()
+//    {
+//        if (panel != null)
+//            panel.SetActive(!panel.activeSelf);
+//        else
+//            Debug.LogWarning("TogglePanelOnClick: panel is not assigned.");
+//    }
+//    public void Quit()
+//    {
+//        Application.Quit();
+//    } 
+//    void Update()
+//    {
+//        // #if UNITY_ANDROID
+//        if (Input.GetKeyDown(KeyCode.Escape))
+//        {
+//            TogglePanel_();
+//        }
+//        // #endif
+//    }
+//}
